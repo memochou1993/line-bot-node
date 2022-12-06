3f63637336239fcf10671ec4f729e735992042d1
Memo Chou <memochou1993@gmail.com>
2022-12-06 23:50:45 +0800

    Add main function

---
+++ index.js
@@ -0,0 +1,65 @@
+const https = require('https');
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+const TOKEN = process.env.LINE_ACCESS_TOKEN;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.post('/webhook', (req, res) => {
+  res.send('HTTP POST request sent to the webhook URL!');
+
+  if (req.body.events[0].type === 'message') {
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${TOKEN}`,
+    };
+
+    const body = JSON.stringify({
+      replyToken: req.body.events[0].replyToken,
+      messages: [
+        {
+          type: 'text',
+          text: 'Hello, user',
+        },
+        {
+          type: 'text',
+          text: 'May I help you?',
+        },
+      ],
+    });
+
+    const webhookOptions = {
+      hostname: 'api.line.me',
+      path: '/v2/bot/message/reply',
+      method: 'POST',
+      headers,
+      body,
+    };
+
+    const request = https.request(webhookOptions, (res) => {
+      res.on('data', (d) => {
+        process.stdout.write(d);
+      });
+    });
+
+    request.on('error', (err) => {
+      console.error(err);
+    });
+
+    request.write(body);
+
+    request.end();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
